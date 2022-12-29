224ca9cfadb13c7bdc87cec9d9b61bfd2c6faa6a
yuikoito <yuiko.dev@gmail.com>
2022-12-30 00:44:09 +0900

    chore: install chakra ui and ogp

---
+++ pages/share/[id].tsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+type Props = {
+  id: string;
+};
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<Props>> => {
+  if (typeof context.params?.id === "string") {
+    return {
+      props: {
+        id: context.params?.id,
+      },
+    };
+  } else {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+const Page = ({ id }: Props) => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "";
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/");
+  }, [router]);
+
+  return (
+    <>
+      <Head>
+        <title>タイトル</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta property="og:url" content={`${baseUrl}/api/ogp?id=${id}`} />
+        <meta
+          name="description"
+          content="絶対にサボってやるという強い意志をお持ちのあなたに"
+        />
+        <meta property="og:title" content="サボりたい" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="絶対にサボってやるという強い意志をお持ちのあなたに"
+        />
+        <meta property="og:site_name" content="サボりたい" />
+        <link rel="icon" href="/favicon.jpg" />
+        <meta name="twitter:site" content="@yui_active" />
+        <meta name="twitter:creator" content="@yui_active" />
+        <meta
+          property="og:image"
+          key="ogImage"
+          content={`${baseUrl}/api/ogp?indexNumbers=${id}`}
+        />
+        <meta
+          name="twitter:card"
+          key="twitterCard"
+          content="summary_large_image"
+        />
+        <meta
+          name="twitter:image"
+          key="twitterImage"
+          content={`${baseUrl}/api/ogp?indexNumbers=${id}`}
+        />
+      </Head>
+    </>
+  );
+};
+export default Page;
